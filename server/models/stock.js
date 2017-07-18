aa29acc95ea38578e64a4fc00bb33620c432d352
Кузнецов <redlocust1@gmail.com>
2017-07-18 14:58:51 +0300

    heroku fix

--- server/models/stock.js
+++ server/models/stock.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/watchstock', (error) => {
+//mongoose.connect('mongodb://localhost:27017/watchstock', (error) => {
+mongoose.connect("mongodb://redlocust:78957895@ds163612.mlab.com:63612/heroku_9zrzpbcf", (error) => {
   if (error) {
     console.error('Please make sure Mongodb is installed and running!');
     throw error;

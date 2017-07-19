e556930542c42147a711ff21a0732c49440f92b6
Кузнецов <redlocust1@gmail.com>
2017-07-19 10:43:01 +0300

    heroku fix

--- server/server.js
+++ server/server.js
@@ -4,43 +4,45 @@ import path from 'path';
 import bodyParser from 'body-parser';
 const app = express();
 const port = process.env.PORT || 3000;
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server);
+//
+// app.use(express.static(path.join(__dirname, '../public')));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// //
+//  app.use('/api', stocks);
+//
+// app.use(function (req, res, next) {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+//
+// io.on('connection', function(socket){
+//   socket.on('ADD_STOCK', function(stockId){
+//     console.log('ADD_STOCK ', stockId);
+//     io.emit('UPDATE', stockId);
+//   });
+// });
+//
+//
+// io.on('connection', function(socket){
+//   socket.on('DELETE_STOCK', function(stockId){
+//     console.log('DELETE_STOCK ', stockId);
+//     io.emit('UPDATE', stockId);
+//   });
+// });
+//
+// server.listen(port, () => {console.log(`listen on ${port}  port`)});
 
-app.use('/api', stocks);
-
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
-io.on('connection', function(socket){
-  socket.on('ADD_STOCK', function(stockId){
-    console.log('ADD_STOCK ', stockId);
-    io.emit('UPDATE', stockId);
-  });
-});
-
-
-io.on('connection', function(socket){
-  socket.on('DELETE_STOCK', function(stockId){
-    console.log('DELETE_STOCK ', stockId);
-    io.emit('UPDATE', stockId);
-  });
-});
-
-server.listen(port, () => {console.log(`listen on ${port}  port`)});
+app.listen(port);
 
 export default app;
